e5949b6cb4f456f989e71aa95613457071515bbe
Eemeli Surakka <eemeli.surakka@hotmail.fi>
2020-09-01 14:05:48 +0200

    let type

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ function add(n1, n2) {
     return n1 + n2;
 }
 ;
-var number1 = '5';
+var number1 = 5;
 var number2 = 3;
+var number3;
+number3 = 'string';
 console.log(add(number1, number2));
--- app.ts
+++ app.ts
@@ -2,7 +2,11 @@ function add (n1: number, n2: number) {
   return n1 + n2
 };
 
-const number1 = '5';
+const number1 = 5;
 const number2 = 3;
+
+// Only assign types to variables that might change.
+let number3: number;
+number3 = 'string';
 
 console.log(add(number1, number2));
